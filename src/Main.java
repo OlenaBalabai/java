2a915744a74a1a5efad9e42225fb46cecfa17525
Elena <balabai.olena@gmail.com>
2018-09-25 23:22:39 +0300

    "HW1 edit"

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,46 @@ public class Main {
 
     public static void main(String[] args) {
         //region Some variables should be described here
+        boolean boolVar;
 
+        boolVar = true;
 
+        System.out.println("boolVar = " + boolVar);
+
+        boolVar = false;
+
+        System.out.println("boolVar = " + boolVar);
+        int a = 101; // decimal-form literal
+        int b = 0100; // octal-form literal
+        int c = 0xFace; // Hexa-decimal form literal
+        int d = 0b1111; // Binary literal
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
         //endregion
 
         //region Some prints of variables should be here
-
+        println(char x);
+        println(char[] x);
+        println(double x);
+        println(float x);
+        println(int x);
+        println(long x);
+        println(Object x);
+        println(String x);
 
         //endregion
 
         System.out.println("Hello World!");
+        /**
+         This
+         is
+         documentation
+         comment
+         */
+
+    // single line comment
+        // /* Multi-line comments */
     }
 }
